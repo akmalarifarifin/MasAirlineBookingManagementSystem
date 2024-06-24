698e805fd386c548f14accece417224477f2fb2f
loqman <loqman.rmi@gmail.com>
2024-06-24 22:15:02 +0800

    model package

--- src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
@@ -5,16 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MasAirlineBookingManagementSystemApplication {
-
-	//testing msater
-
 	public static void main(String[] args) {
 		SpringApplication.run(MasAirlineBookingManagementSystemApplication.class, args);
-		//loqmantesttest
-		//comment2
-		//test akmalassds after commit
-		//commit4
-		//Hello
 	}
-
 }
