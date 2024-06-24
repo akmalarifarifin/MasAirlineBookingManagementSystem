bec662aef22ee150bf42f27a8fce7b2ffff6646b
loqman <loqman.rmi@gmail.com>
2024-06-24 21:41:31 +0800

    second test loqman

--- src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
@@ -11,6 +11,7 @@ public class MasAirlineBookingManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MasAirlineBookingManagementSystemApplication.class, args);
 		//loqmantesttest
+		//comment2
 	}
 
 }
