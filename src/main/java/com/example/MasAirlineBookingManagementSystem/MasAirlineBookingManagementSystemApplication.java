6dfa3d5ff9bf211bba81976a8f2ed67d1f4094a6
akmal.arif <akmalarifarifin@gmail.com>
2024-06-24 21:08:06 +0800

    Initial commit

---
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.MasAirlineBookingManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasAirlineBookingManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasAirlineBookingManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MasAirlineBookingManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasAirlineBookingManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
