49ce5b1bc4a5bd7b417441e747a33e0a0d34230a
akmal.arif <akmalarifarifin@gmail.com>
2024-06-24 21:25:00 +0800

    test 2nd commit

--- src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/MasAirlineBookingManagementSystemApplication.java
@@ -8,6 +8,7 @@ public class MasAirlineBookingManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasAirlineBookingManagementSystemApplication.class, args);
+		//test akmal branchg
 	}
 
 }
