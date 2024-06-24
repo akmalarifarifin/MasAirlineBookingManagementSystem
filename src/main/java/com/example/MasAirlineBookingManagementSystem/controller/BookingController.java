04561ba92db3f6e95b4fc0cee4872ba2ed210835
akmal.arif <akmalarifarifin@gmail.com>
2024-06-24 22:12:04 +0800

    new controller

---
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/controller/BookingController.java
@@ -0,0 +1,12 @@
+package com.example.MasAirlineBookingManagementSystem.controller;
+
+public class BookingController {
+
+    void createBooking (){
+        //create booking
+        //test for conflict
+        //test for conflict
+        //test for conflict
+        //test for conflict
+    }
+}
