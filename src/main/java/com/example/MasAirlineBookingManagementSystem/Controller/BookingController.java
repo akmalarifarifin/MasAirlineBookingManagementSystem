49db7623d2cdc40fe789ca6b71537425aac8b5a7
akmal.arif <akmalarifarifin@gmail.com>
2024-06-25 22:12:55 +0800

    Combine awanis and akmals branch

--- src/main/java/com/example/MasAirlineBookingManagementSystem/Controller/BookingController.java
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/Controller/BookingController.java
@@ -1,6 +1,7 @@
 package com.example.MasAirlineBookingManagementSystem.Controller;
 
 import com.example.MasAirlineBookingManagementSystem.Model.Booking;
+import com.example.MasAirlineBookingManagementSystem.Service.BookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class BookingController {
 
     @GetMapping
     public List<Booking> getAllBooking(){
-        return bookingService.getAllBooking();
+        return bookingService.getAllBookings();
     }
 
     @GetMapping("/{id}")
