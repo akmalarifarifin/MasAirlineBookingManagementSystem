cb5bae3971552579519aec2fb56091c1a5aad071
famell <famell.qawiem9@gmail.com>
2024-06-29 11:00:18 +0800

    11:47pm 25/06/2024 // Comment: User web update. Edit/delete/view

--- src/main/java/com/example/MasAirlineBookingManagementSystem/Controller/UserWebController.java
+++ src/main/java/com/example/MasAirlineBookingManagementSystem/Controller/UserWebController.java
@@ -10,47 +10,52 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/web/users")
+@RequestMapping
 public class UserWebController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping
+    @GetMapping("/web/users")
     public String getAllUsers(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
-        return "listUser";
+        return "listUsers";
     }
 
-    @GetMapping("/new")
+    @GetMapping("/web/users/new")
     public String showCreateUserForm(Model model) {
         model.addAttribute("user", new User());
         return "createUser";
     }
 
-    @PostMapping("/new")
+    @PostMapping("/usernew")
     public String createUser(@ModelAttribute User user) {
         userService.createUser(user);
         return "redirect:/web/users";
     }
 
-    @GetMapping("/{id}/edit")
+    @GetMapping("/web/users/{id}/edit")
     public String editUserForm(@PathVariable("id") Long id, Model model) {
         User user = userService.getUserById(id);
         model.addAttribute("user", user);
         return "editUser";
     }
 
-    @PostMapping("/{id}/update")
+    @PostMapping("/web/users/{id}/update")
     public String updateUser(@PathVariable("id") Long id, @ModelAttribute User updatedUser) {
         userService.updateUser(id, updatedUser);
         return "redirect:/web/users";
     }
 
-    @GetMapping("/{id}/delete")
+    @GetMapping("/web/users/{id}/delete")
     public String deleteUser(@PathVariable("id") Long id) {
         userService.deleteUser(id);
         return "redirect:/web/users";
     }
+
+   /* @GetMapping("/main-page")
+    public String showMainPage() {
+        return "mainUserPage";
+    }*/
 }
